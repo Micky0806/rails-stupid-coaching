82299c5adda7db45e64bb471185c33bbb7e093ae
Mike Hanot <mikehanot@gmail.com>
2016-05-30 14:29:42 +0200

    first excercice finish

--- app/controllers/coaching_controller.rb
+++ app/controllers/coaching_controller.rb
@@ -1,9 +1,15 @@
 class CoachingController < ApplicationController
   def answer
-
+    @querry = params[:querry]
+    if @querry.end_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
+      elsif @querry == "I am going to work right now!"
+        @answer =  "Ok great"
+    else
+      @answer =  "I don't care, get dressed and go to work!"
+    end
   end
 
-  def ask
-
-  end
+def ask
+end
 end
